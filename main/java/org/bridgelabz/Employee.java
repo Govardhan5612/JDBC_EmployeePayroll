b0826a471e050d6ce62e84a1da34fb592eaab45c
Govardhan5612 <govardhansiripireddy5612@gmail.com>
2023-01-12 00:14:44 +0530

    Read the employee payroll table in payroll service database

--- main/java/org/bridgelabz/Employee.java
+++ main/java/org/bridgelabz/Employee.java
@@ -3,6 +3,7 @@ package org.bridgelabz;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.Scanner;
 
 /**
@@ -22,22 +23,51 @@ public class Employee {
          * In this method edit the terisa basic pay details
          */
         try {
+            EmployeePayroll employee = new EmployeePayroll();
             Connection connection = DriverManager.getConnection(jdbc, userName, password);
             PreparedStatement prepare = connection.prepareStatement("update employee_payroll set BasicPay = ? where Employee_id = ?;");
             System.out.print("Enter id number : ");
-            String id = input.next();
+            employee.setEmployee_id(input.nextInt());
             System.out.print("Enter basic pay : ");
-            int name = input.nextInt();
-            prepare.setInt(1, name);
-            prepare.setString(2, id);
+            employee.setBasicPay(input.nextInt());
+            prepare.setInt(1, employee.getBasicPay());
+            prepare.setInt(2, employee.getEmployee_id());
             prepare.executeLargeUpdate();
         } catch (Exception exception) {
             exception.printStackTrace();
         }
     }
 
+    public static void read() {
+        /**
+         * In this method read the payroll service database
+         */
+        EmployeePayroll employee = new EmployeePayroll();
+        System.out.print("Enter name : ");
+        employee.setName(input.next());
+        try {
+            Connection connection = DriverManager.getConnection(jdbc, userName, password);
+            String query = "select * from employee_payroll where name=?";
+            PreparedStatement prepare = connection.prepareStatement(query);
+            prepare.setString(1, employee.getName());
+            ResultSet result = prepare.executeQuery();
+            while (result.next()) {
+                System.out.print("id : ");
+                System.out.print(result.getString("employee_id"));
+                System.out.print(" , Name : ");
+                System.out.print(result.getString("name"));
+
+                System.out.print(" , basic pay : ");
+                System.out.print(result.getInt("basicPay"));
+                System.out.println();
+            }
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) {
-        edit();
+        read();
     }
 
 }
